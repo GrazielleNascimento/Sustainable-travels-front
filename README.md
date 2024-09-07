aa837a5ab2b8883859e895d63395cd6feac5ecff
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-09-07 20:24:25 -0300

    feat:📝 Add Vite configuration and basic project structure
    This commit adds the Vite configuration file (vite.config.js) and sets up the basic project structure. The Vite configuration includes the React plugin and defines the plugins array. The project structure includes the README.md file with instructions for running the project in development mode, building the project for production, and deploying the project. It also includes the src directory with the main App component (App.jsx), the main entry point (main.jsx), and the CSS file for the App component (App.css). Additionally, it includes the index.html file with the root element and the script tag to load the main.jsx file. Finally, it includes the .gitignore file, the package.json file with the project dependencies and scripts, the eslint.config.js file with the ESLint configuration, and the index.css file with the global CSS styles.

---
+++ README.md
@@ -0,0 +1,15 @@
+# React + Vite
+
+## Como Rodar o Projeto em modo DEV?
+
+npm run dev
+
+## Como buildar o Projeto para Prod?
+
+npm run build
+
+## Como fazer Deploy do Projeto?
+
+npx vercel login
+
+npx vercel ./dist --prod
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([
+    
+  ])
+
+  return (
+    <>
+      <div>
+    
+      </div>
+    </>
+  )
+}
+
+export default App
